252ff6211d04e946250580907fc33e341eec8504
luowei <wei.luo@karakal.com.cn>
2016-05-31 14:45:06 +0800

    update

---
+++ app/src/main/java/com/luowei/opentools/module/circleimageview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.luowei.opentools.module.circleimageview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.luowei.opentools.BaseActivity;
+import com.luowei.opentools.R;
+
+/**
+ * Created by 骆巍 on 2016/5/31.
+ */
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.circleimage_ac_main);
+    }
+}
